45569af2d2209e8c17861c5c9948b740a46d09a7
piotruela <piotr.maszota@leancode.co>
2024-11-10 19:42:40 +0100

    Use snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:universal_html/html.dart' hide VoidCallback;
+
+/// Registers [effect] to be run in
+/// WidgetsBinding.instance.addPostFrameCallback.
+void usePostFrameEffect(VoidCallback effect, [List<Object?>? keys]) {
+  useEffect(
+    () {
+      WidgetsBinding.instance.addPostFrameCallback((_) => effect());
+      return null;
+    },
+    keys,
+  );
+}
 
 void main() {
+  mainCommon();
+}
+
+Future<void> mainCommon() async {
+  final prefs = await SharedPreferences.getInstance();
+  final response = await HttpRequest.getString(
+    'version.txt?${DateTime.now().millisecondsSinceEpoch}',
+  );
+  final currentVersion = response.trim();
+  final savedVersion = prefs.getString('version');
+
   runApp(
-    const MaterialApp(
-      home: MainApp(),
+    MaterialApp(
+      home: MainApp(
+        shouldReload: currentVersion != savedVersion,
+      ),
     ),
   );
 }
@@ -12,13 +39,43 @@ void main() {
 class MainApp extends HookWidget {
   const MainApp({
     super.key,
+    required this.shouldReload,
   });
+
+  final bool shouldReload;
 
   @override
   Widget build(BuildContext context) {
+    usePostFrameEffect(() {
+      if (shouldReload) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('New version available!'),
+            content:
+                const Text('Please reload the page to get the latest version.'),
+            actions: [
+              TextButton(
+                onPressed: () async {
+                  final prefs = await SharedPreferences.getInstance();
+                  final response = await HttpRequest.getString(
+                    'version.txt?${DateTime.now().millisecondsSinceEpoch}',
+                  );
+                  final currentVersion = response.trim();
+                  prefs.setString('version', currentVersion);
+                  window.location.reload();
+                },
+                child: const Text('Reload'),
+              ),
+            ],
+          ),
+        );
+      }
+    });
+
     return const Scaffold(
       body: Center(
-        child: Text('Hello World! 20'),
+        child: Text('Hello World! 21'),
       ),
     );
   }
